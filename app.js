599c585aad071c6875ffe03ecabba59ed5994496
Kyle <kfkyleforbes@gmail.com>
2019-05-22 20:49:37 -0700

    practice start

---
+++ app.js
@@ -0,0 +1,9 @@
+// Person constructor
+function Person(name) {
+    this.name = name;
+    this.age = age;
+    console.log(this);
+}
+
+const kyle = new Person('Kyle', 26);
+const john = new Person('John', 30);
